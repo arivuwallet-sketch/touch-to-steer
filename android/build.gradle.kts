3e75c618a141408e1196d03e7e8cd2341c7dbdfc
arivuwallet-sketch <arivuwallet@gmail.com>
2026-09-22 07:24:32 +0530

    feat: add native Flutter Android controller

---
+++ android/build.gradle.kts
@@ -0,0 +1,17 @@
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val sharedBuildDir = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(sharedBuildDir)
+
+subprojects {
+    project.layout.buildDirectory.value(sharedBuildDir.dir(project.name))
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
